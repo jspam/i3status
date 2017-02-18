89e52e88a2eabe19d558a36b96a00ead827243a7
jspam <jspam@users.noreply.github.com>
2017-02-18 22:26:23 +0100

    Add support for German colloquial time format to time module

--- src/print_time.c
+++ src/print_time.c
@@ -16,6 +16,10 @@ static bool local_timezone_init = false;
 static const char *local_timezone = NULL;
 static const char *current_timezone = NULL;
 
+static const char *volle_stunde[] = { "Zwölf", "Ein", "Zwei", "Drei", "Vier", "Fünf", "Sechs", "Sieben", "Acht", "Neun", "Zehn", "Elf" };
+static const char *stunde[] = { "zwölf", "eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn", "elf" };
+static const char *minute[] = { "Fünf nach", "Zehn nach", "Viertel nach", "Zwanzig nach", "Fünf vor halb", "Halb", "Fünf nach halb", "Zehn nach halb", "Viertel vor", "Zehn vor", "Fünf vor" };
+
 void set_timezone(const char *tz) {
     if (!local_timezone_init) {
         /* First call, initialize. */
@@ -62,13 +66,26 @@ void print_time(yajl_gen json_gen, char *buffer, const char *title, const char *
     }
 
     char string_time[STRING_SIZE];
+    char colloq_time[STRING_SIZE];
 
     if (format_time == NULL) {
         outwalk += strftime(buffer, 4096, format, &tm);
     } else {
-        strftime(string_time, sizeof(string_time), format_time, &tm);
+        strftime(string_time, sizeof(string_time), format_time, &tm); 
+
+        const time_t t2 = t + 150;
+        struct tm tm2;
+        localtime_r(&t2, &tm2);
+
+        if (tm2.tm_min < 5) {
+            snprintf(colloq_time, sizeof(colloq_time), "%s Uhr", volle_stunde[tm2.tm_hour % 12]);
+        } else {
+            snprintf(colloq_time, sizeof(colloq_time), "%s %s", minute[tm2.tm_min / 5 - 1], stunde[(tm2.tm_hour + (tm2.tm_min >= 25)) % 12]);
+        }
+
         placeholder_t placeholders[] = {
-            {.name = "%time", .value = string_time}};
+            {.name = "%time", .value = string_time},
+            {.name = "%text", .value = colloq_time}};
 
         const size_t num = sizeof(placeholders) / sizeof(placeholder_t);
         buffer = format_placeholders(format_time, &placeholders[0], num);
